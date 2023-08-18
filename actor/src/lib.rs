a2f359bc0784511eb2ab13d27f93178a6a038ece
Taylor Thomas <taylor@cosmonic.com>
2023-08-18 14:44:32 -0600

    An actor
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

---
+++ actor/src/lib.rs
@@ -0,0 +1,25 @@
+use exports::wasmcloud::messaging::handler::{BrokerMessage, Handler};
+
+wit_bindgen::generate!({
+    world: "messaging",
+    exports: {
+        "wasmcloud:messaging/handler": MessagingWitTestActor,
+    }
+});
+
+struct MessagingWitTestActor {}
+
+impl Handler for MessagingWitTestActor {
+    /// handle subscription response
+    fn handle_message(msg: BrokerMessage) -> Result<(), String> {
+        // if the sender wants a reply
+        if let Some(reply_to) = msg.reply_to {
+            wasmcloud::messaging::consumer::publish(&BrokerMessage {
+                body: msg.body,
+                reply_to: None,
+                subject: reply_to,
+            })?;
+        }
+        Ok(())
+    }
+}
